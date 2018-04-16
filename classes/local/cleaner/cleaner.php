75e4910ffff13e5116940369220b34c08ff9965e
Jonny Barnes <jonathan.barnes@buildempire.co.uk>
2018-04-16 12:32:11 +0100

    Add course searchable type to cleaned urls, and remove these from the clean url before passing it in to the clean function

--- classes/local/cleaner/cleaner.php
+++ classes/local/cleaner/cleaner.php
@@ -184,8 +184,12 @@ class cleaner
         }
 
         $course = get_course($this->params['id']);
+        $type_id = $DB->get_record_sql('SELECT type FROM {cscore_course_information} WHERE course_id = :course_id', [
+            'course_id' => $course->id,
+        ])->type;
+        $type = \local_cscore\Information::COURSE_TYPE_SEARCHABLES[$type_id][0];
 
-        $newpath = '/course/' . urlencode($course->shortname);
+        $newpath = '/course/' . urlencode($type) , '/' . urlencode($course->shortname);
         if ($this->check_path_allowed($newpath)) {
             $this->path = $newpath;
             unset($this->params['id']);
@@ -205,8 +209,12 @@ class cleaner
 
         $courseid = $DB->get_field('course', 'id', ['shortname' => $this->params['name']]);
         $course   = get_course($courseid);
+        $type_id = $DB->get_record_sql('SELECT type FROM {cscore_course_information} WHERE course_id = :course_id', [
+            'course_id' => $course->id,
+        ])->type;
+        $type = \local_cscore\Information::COURSE_TYPE_SEARCHABLES[$type_id][0];
 
-        $newpath = '/course/' . urlencode($course->shortname);
+        $newpath = '/course/' . urlencode($type) , '/' . urlencode($course->shortname);
         if ($this->check_path_allowed($newpath)) {
             $this->path = $newpath;
             unset($this->params['name']);
